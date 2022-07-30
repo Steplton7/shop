23be0540c73670a94fd4416c36c0a3077689f69a
Steplton7 <107725240+Steplton7@users.noreply.github.com>
2022-07-30 03:06:28 +0300

    Complete link category on top menu and middle menu

--- shop/suits/templatetags/menu_tags.py
+++ shop/suits/templatetags/menu_tags.py
@@ -5,6 +5,9 @@ from suits.models import Category
 register = template.Library()
 
 
+def get_all_categories():
+    return Category.objects.all()
+
 #@register.simple_tag()
 #def get_categories():
 #    """Вывод всех категорий"""
@@ -13,6 +16,11 @@ register = template.Library()
 
 @register.inclusion_tag('suits/include/tags/top_menu.html')
 def get_categories():
-    category = Category.objects.all().order_by("name")
+    category = get_all_categories()
     return {"list_category": category}
 #filter(parent__isnull=True)
+
+@register.inclusion_tag('suits/include/tags/middle_menu.html')
+def get_menu_categories():
+    category = Category.objects.filter(level=1).order_by('name')
+    return {"list_category": category}
